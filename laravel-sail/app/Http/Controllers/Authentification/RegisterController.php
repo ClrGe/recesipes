4ba1c6bc9d32a4d02c41f1d5801841c31f9e7c9d
NoahSmo <90006474+NoahSmo@users.noreply.github.com>
2022-06-23 10:52:15 +0200

    Ajout page Contact, Login, Register Ajout Cartes pour recette home page

--- laravel-sail/app/Http/Controllers/Authentification/RegisterController.php
+++ laravel-sail/app/Http/Controllers/Authentification/RegisterController.php
@@ -12,7 +12,7 @@ class RegisterController extends Controller
         return view('Authentification/register');
     }
 
-    public function RegisterUser()
+    public function Register()
     {
 
     }
--- laravel-sail/routes/web.php
+++ laravel-sail/routes/web.php
@@ -19,10 +19,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name("home");
+
+    //Route::get('/login', [LogController::class, 'index'])->name('login');
+    //Route::post('/login', [LogController::class, 'login'])->name('login');
+    //
+    //Route::get('/logout', [LogController::class, 'logout'])->name('logout');
+    //
+    //Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    //Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+
 Route::get('/contact', function(){
     return view('contact');
 })->name('contact.form');
 
-Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.list');
-Route::get('/login', [LogController::class, 'index'])->name('login.form');
-Route::get('/register', [RegisterController::class, 'index'])->name('register.form');
+
+Route::resource('recipe', RecipeController::class);
+Route::resource('login', LogController::class);
+Route::resource('register', RegisterController::class);
+
+
+//Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.list');
+//Route::get('/login', [LogController::class, 'index'])->name('login.form');
+//Route::get('/register', [RegisterController::class, 'index'])->name('register.form');
