bdcadd7b775f08ecbf546b6ecf248df1e869d2b1
Slupshi <slupshi@gmail.com>
2022-06-29 20:52:00 +0200

    feat: API GET Ingredients/Medias/Roles/Permissions

---
+++ laravel-sail/app/Http/Controllers/API/MediaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Recipes\Media;
+use App\Models\Recipes\Recipe;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class MediaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Response::json(Media::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Recipes\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Media $media)
+    {
+        return Response::json($media);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Recipes\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Media $media)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Recipes\Media  $media
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Media $media)
+    {
+        //
+    }
+
+    public function byRecipe(Recipe $recipe)
+    {
+        $medias = Media::all()->where("recipe_id", "=", $recipe->id);
+        return Response::json($medias);
+    }
+}
--- laravel-sail/routes/api.php
+++ laravel-sail/routes/api.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\EvaluationController;
+use App\Http\Controllers\API\IngredientController;
+use App\Http\Controllers\API\MediaController;
+use App\Http\Controllers\API\PermissionController;
 use App\Http\Controllers\API\QuantityController;
 use App\Http\Controllers\API\RecipeController;
+use App\Http\Controllers\API\RoleController;
 use App\Http\Controllers\API\SearchController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
@@ -25,37 +29,64 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 #region Recipes
-Route::get('recipes/favorites', [RecipeController::class, 'favorites'])->middleware('auth:api')->name('api.recipes.favorites');
-Route::get('recipes/myrecipes', [RecipeController::class, 'myRecipes'])->middleware('auth:api')->name('api.recipes.myrecipes');
-Route::get('recipes/lastrecipes', [RecipeController::class, 'lastRecipes'])->middleware('auth:api')->name('api.recipes.lastrecipes');
-Route::get('recipes/random', [RecipeController::class, 'random'])->middleware('auth:api')->name('api.recipes.random');
-Route::get('recipes/{recipe}', [RecipeController::class, 'show'])->middleware('auth:api')->name('api.recipes.show');
-Route::get('recipes', [RecipeController::class, 'index'])->middleware('auth:api')->name('api.recipes.index');
-Route::get('recipes/search/{substring}', [RecipeController::class, 'search'])->middleware('auth:api')->name('api.recipes.search');
 
-#endregion
+    #region Recipe
+    Route::get('recipes/favorites', [RecipeController::class, 'favorites'])->middleware('auth:api')->name('api.recipes.favorites');
+    Route::get('recipes/myrecipes', [RecipeController::class, 'myRecipes'])->middleware('auth:api')->name('api.recipes.myrecipes');
+    Route::get('recipes/lastrecipes', [RecipeController::class, 'lastRecipes'])->middleware('auth:api')->name('api.recipes.lastrecipes');
+    Route::get('recipes/random', [RecipeController::class, 'random'])->middleware('auth:api')->name('api.recipes.random');
+    Route::get('recipes/{recipe}', [RecipeController::class, 'show'])->middleware('auth:api')->name('api.recipes.show');
+    Route::get('recipes', [RecipeController::class, 'index'])->middleware('auth:api')->name('api.recipes.index');
+    Route::get('recipes/search/{substring}', [RecipeController::class, 'search'])->middleware('auth:api')->name('api.recipes.search');
+    #endregion
 
-#region Category
-Route::get('categories', [CategoryController::class, 'index'])->middleware('auth:api')->name("api.categories.index");
-Route::get('categories/{category}', [CategoryController::class, 'show'])->middleware('auth:api')->name("api.categories.show");
-Route::get('categories/search/{substring}', [CategoryController::class, 'search'])->middleware('auth:api')->name("api.categories.search");
+    #region Ingredients
+    Route::get('ingredients', [IngredientController::class, 'index'])->middleware('auth:api')->name('api.ingredients.index');
+    Route::get('ingredients/{ingredient}', [IngredientController::class, 'show'])->middleware('auth:api')->name('api/ingredients.show');
+    #endregion
 
-#endregion
+    #region Category
+    Route::get('categories', [CategoryController::class, 'index'])->middleware('auth:api')->name("api.categories.index");
+    Route::get('categories/{category}', [CategoryController::class, 'show'])->middleware('auth:api')->name("api.categories.show");
+    Route::get('categories/search/{substring}', [CategoryController::class, 'search'])->middleware('auth:api')->name("api.categories.search");
+    #endregion
 
-#region Quantities
-Route::get('quantities', [QuantityController::class, 'index'])->middleware('auth:api')->name('api.quantities.index');
-Route::get('quantities/{quantity}', [QuantityController::class, 'show'])->middleware('auth:api')->name('api.quantities.show');
-#endregion
+    #region Quantities
+    Route::get('quantities', [QuantityController::class, 'index'])->middleware('auth:api')->name('api.quantities.index');
+    Route::get('quantities/{quantity}', [QuantityController::class, 'show'])->middleware('auth:api')->name('api.quantities.show');
+    #endregion
 
-#region Evaluations
-Route::get('evaluations', [EvaluationController::class, 'index'])->middleware('auth:api')->name('api.evaluations.index');
-Route::get('evaluations/{evaluation}', [EvaluationController::class, 'show'])->middleware('auth:api')->name('api.evaluations.show');
-Route::get('evaluations/recipe/{recipe}', [EvaluationController::class, 'byRecipe'])->middleware('auth:api')->name('api.evaluations.byrecipe');
+    #region Evaluations
+    Route::get('evaluations', [EvaluationController::class, 'index'])->middleware('auth:api')->name('api.evaluations.index');
+    Route::get('evaluations/{evaluation}', [EvaluationController::class, 'show'])->middleware('auth:api')->name('api.evaluations.show');
+    Route::get('evaluations/recipe/{recipe}', [EvaluationController::class, 'byRecipe'])->middleware('auth:api')->name('api.evaluations.byrecipe');
+    #endregion
+
+    #region Media
+    Route::get('medias', [MediaController::class, 'index'])->middleware('auth:api')->name('api.medias.index');
+    Route::get('medias/{media}', [MediaController::class, 'show'])->middleware('auth:api')->name('api.medias.show');
+    Route::get('medias/recipe/{recipe}', [MediaController::class, 'byRecipe'])->middleware('auth:api')->name('api.medias.byrecipe');
+    #endregion
+
 #endregion
 
 #region Users
-Route::get('users', [UserController::class, 'index'])->middleware('auth:api')->name('api.users.index');
-Route::get('users/{user}', [UserController::class, 'show'])->middleware('auth:api')->name('api.users.show');
+
+    #region User
+    Route::get('users', [UserController::class, 'index'])->middleware('auth:api')->name('api.users.index');
+    Route::get('users/{user}', [UserController::class, 'show'])->middleware('auth:api')->name('api.users.show');
+    #endregion
+
+    #region Roles
+    Route::get('roles', [RoleController::class, 'index'])->middleware('auth:api')->name('api.roles.index');
+    Route::get('roles/{role}', [RoleController::class, 'show'])->middleware('auth:api')->name('api.roles.show');
+    #endregion
+
+    #region Permissions
+    Route::get('permissions', [PermissionController::class, 'index'])->middleware('auth:api')->name('api.permissions.index');
+    Route::get('permissions/{permission}', [PermissionController ::class, 'show'])->middleware('auth:api')->name('api.permissions.show');
+    #endregion
+
 #endregion
 
 Route::get('search/{substring}', [SearchController::class, 'search'])->name('api.search');
