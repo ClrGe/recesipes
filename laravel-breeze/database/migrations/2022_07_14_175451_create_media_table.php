eda489e81de2c9b2128574ee4b9f537d3587ff20
Slupshi <slupshi@gmail.com>
2022-07-12 23:09:12 +0200

    feat: change FK check method in migrations

--- laravel-breeze/database/migrations/2022_07_14_175451_create_media_table.php
+++ laravel-breeze/database/migrations/2022_07_14_175451_create_media_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,11 +16,11 @@ return new class extends Migration
     {
         Schema::create('media', function (Blueprint $table) {
             $table->id();
+            $table->text("description");
             $table->string("alt");
             $table->string("path");
             $table->unsignedBigInteger("recipe_id");
             $table->timestamps();
-
         });
     }
 
--- laravel-breeze/database/migrations/2022_07_16_090948_create_shopping_list_table.php
+++ laravel-breeze/database/migrations/2022_07_16_090948_create_shopping_list_table.php
@@ -28,6 +28,8 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('shopping_list');
+        Schema::enableForeignKeyConstraints();
     }
 };
