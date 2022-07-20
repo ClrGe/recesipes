d988ae1010eeddf2a926521ccae5c7892a4942ac
Slupshi <slupshi@gmail.com>
2022-07-20 16:10:04 +0200

    feat: correction cancel button on users

--- laravel-breeze/resources/views/backoffice/users.blade.php
+++ laravel-breeze/resources/views/backoffice/users.blade.php
@@ -102,7 +102,7 @@
                         <td></td>
                         <td>
                             <div class="p-6 bg-white capitalize">                                
-                                <a class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" href="{{ route('backoffice.roles') }}">
+                                <a class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full" href="{{ route('backoffice.users') }}">
                                     Cancel
                                 </a>                                      
                             </div>
